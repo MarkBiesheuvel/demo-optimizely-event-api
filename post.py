5d3ccd0384d3e0e584abc5e32f6eadbe67c18467
Mark Biesheuvel <mail@markbiesheuvel.nl>
2022-06-08 09:59:20 +0200

    Add conversion event via Events API

--- post.py
+++ post.py
@@ -2,13 +2,50 @@ from json import loads as json_decode, dumps as json_encode
 from requests import post
 from uuid import uuid4
 from time import time
+from random import random
 
 URL = 'https://logx.optimizely.com/v1/events'
+
+DESIRED_CONVERSION_RATES = [0.2, 0.3, 0.2, 0.4, 0.3]
 
 
 def get_data_file():
     with open('datafile.json', 'r') as file:
         return file.read()
+
+
+def get_decisions(campaign_id, experiment_id, variation_id):
+    decisions = [
+        {
+            'campaign_id': campaign_id,
+            'experiment_id': experiment_id,
+            'variation_id': variation_id,
+            'is_campaign_holdback': False
+        }
+    ]
+
+    return decisions
+
+
+def get_events(campaign_id, event_id, event_key, timestamp, conversion_rate):
+    events = [
+        {
+            'entity_id': campaign_id,
+            'type': 'campaign_activated',
+            'timestamp': timestamp,
+            'uuid': str(uuid4())
+        }
+    ]
+
+    if random() < conversion_rate:
+        events.append({
+            'entity_id': event_id,
+            'key': event_key,
+            'timestamp': timestamp,
+            'uuid': str(uuid4())
+        })
+
+    return events
 
 
 def main():
@@ -17,6 +54,9 @@ def main():
     timestamp = int(time() * 1000)
 
     account_id = datafile['accountId']
+    event = datafile['events'][0] # Only one event
+    event_id = event['id']
+    event_key = event['key']
     experiment = datafile['experiments'][0] # Only one experiment
     experiment_id = experiment['id']
     campaign_id = experiment['layerId']
@@ -30,27 +70,23 @@ def main():
                 'visitor_id': str(visitor_id),
                 'snapshots': [
                     {
-                        'decisions': [
-                            {
-                                'campaign_id': campaign_id,
-                                'experiment_id': experiment_id,
-                                'variation_id': variation_ids[visitor_id % number_of_variations],
-                                'is_campaign_holdback': False
-                            }
-                        ],
-                        'events': [
-                            {
-                                'entity_id': campaign_id,
-                                'type': 'campaign_activated',
-                                'timestamp': timestamp,
-                                'uuid': str(uuid4()),
-                            }
-                        ]
+                        'decisions': get_decisions(
+                            campaign_id,
+                            experiment_id,
+                            variation_ids[visitor_id % number_of_variations]
+                        ),
+                        'events': get_events(
+                            campaign_id,
+                            event_id,
+                            event_key,
+                            timestamp,
+                            DESIRED_CONVERSION_RATES[visitor_id % number_of_variations]
+                        )
                     }
                 ]
             }
             # Note if this number gets too big, the request might exceed the 10MB limit
-            for visitor_id in range(1500, 2000)
+            for visitor_id in range(4000, 5000)
         ],
         'enrich_decisions': True,
         'anonymize_ip': True,
@@ -58,6 +94,7 @@ def main():
         'client_version': '0.0.1'
     }
 
+    # print(payload)
     response = post(URL, json=payload)
     print(response.status_code)
 
